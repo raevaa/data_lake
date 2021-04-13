fb7c47e59a2a46be5463ff151c2eea98e8ea08bc
raevaa <79787154+raevaa@users.noreply.github.com>
2021-04-13 13:38:11 +0300

    Add files via upload

---
+++ git_sync.py
@@ -0,0 +1,34 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.latest_only_operator import LatestOnlyOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 2, 18, 8, 20, 0),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=20),
+}
+
+dag = DAG(
+    'git_sync',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(seconds=60),
+)
+
+latest_only = LatestOnlyOperator(task_id='latest_only', dag=dag)
+
+git_pull = BashOperator(
+    task_id='git_pull',
+    bash_command='git -C /root/airflow/dags/ pull',
+    dag=dag,
+)
+
+latest_only >> git_pull
